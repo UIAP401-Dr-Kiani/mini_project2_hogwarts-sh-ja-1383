3e591e579f6a0bbbd0263750b7442789d782a587
Shahla <shahla.jafari.ui.ce.1401@gmail.com>
2023-04-23 10:57:54 +0430

    The file was created and the person class was completed.

---
+++ Harry Potter/Harry Potter/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harry_Potter
+{
+    internal class Person : Authorized_Persons
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearOfBirth { get; set; }
+        enum Gender { Male, Female }
+        public string FatherName { get; set; }
+        public int UserName { get; set; }
+        public int Password { get; set; }
+        enum RaceType
+        {
+            Half_blood ,
+            Pure_blood ,
+            Muggle_blood
+        }
+    }
+}
