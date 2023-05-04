5e4c3c265c6f615f758341e2db7106febf89335b
Shahla <shahla.jafari.ui.ce.1401@gmail.com>
2023-05-04 15:34:19 +0430

    file and start program

--- Harry Potter/Harry Potter/Program.cs
+++ Harry Potter/Harry Potter/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Harry_Potter
 {
@@ -11,8 +14,54 @@ namespace Harry_Potter
     {
         static void Main(string[] args)
         {
+            using (StreamReader file = new StreamReader("TXT_DATA.tsv"))
+            {
+                string ln;
+                while ((ln = file.ReadLine()) != null)
+                {
+                    string[] human = ln.Split('\t').ToArray<string>();
+                    
+                    while (true)
+                    {
+                        Console.WriteLine(
+                        "Choose one:\n" +
+                        "1.Dumbeldore\n" +
+                        "2.Teacher\n" +
+                        "3.Student of Hogwarts\n" +
+                        "4.Invited/Waiting for Invitation to Hogwarts");
+
+                        int choose = Convert.ToInt32(Console.ReadLine());
+                        switch (choose)
+                        {
+                            case 1:
+                            {
+
+                                break;
+                            }
+                            case 2:
+                            {
+                                break;
+                            }
+                            case 3:
+                            {
+                                break;
+                            }
+                            case 4:
+                            {
+                                break;
+                            }
+
+                        }
+
+                    }
+                }
+                
+                file.Close();
+            }
             
             
+            Console.ReadKey();
+
         }
     }
 }
